8a54c6bc30bd51aac2023fc3a985dc12914ff57e
kial917 <kial917@mail.ru>
2021-05-21 19:04:35 +0300

    16.3 minor fix and get render task's array

--- tdd-utils.js
+++ tdd-utils.js
@@ -119,13 +119,11 @@ const arr = [{
         name: "Топ Ган: Мэверик"
     },
 ].map(function (film) {
-    if (parseInt(film.rating, 10)>=92){
-        film.cost=film.cost*2;}
-    const objElem={};
-    objElem.rating=film.rating;
-    objElem.cost=film.cost;
-    objElem.name=film.name;
-    
+    (parseInt(film.rating, 10))?(film.cost = film.cost * 2):film.cost;
+    const objElem = {};
+    objElem.rating = film.rating;
+    objElem.cost = film.cost;
+    objElem.name = film.name;
     return objElem;
 
     /**
@@ -138,105 +136,107 @@ const arr = [{
  * Отфильтруйте фильмы с рейтингом ниже 90%.
  * Почитать про фильтр можно тут - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
  */
- const arr1 = [{
-    rating: "96%",
-    cost: 41.3,
-    name: "Дюна"
-},
-{
-    rating: "96%",
-    cost: 32.4,
-    name: "Звёздный путь 4"
-},
-{
-    rating: "96%",
-    cost: 39.6,
-    name: "Доктор Стрэндж и мультивселенная безумия"
-},
-{
-    rating: "96%",
-    cost: 33.7,
-    name: "Круэлла"
-},
-{
-    rating: "96%",
-    cost: 33.0,
-    name: "Смерть на Ниле"
-},
-{
-    rating: "95%",
-    cost: 38.3,
-    name: "Вечные"
-},
-{
-    rating: "94%",
-    cost: 39.2,
-    name: "Матрица 4"
-},
-{
-    rating: "94%",
-    cost: 40.8,
-    name: "Главный герой"
-},
-{
-    rating: "94%",
-    cost: 41.2,
-    name: "Морбиус"
-},
-{
-    rating: "93%",
-    cost: 32.1,
-    name: "Веном 2"
-},
-{
-    rating: "93%",
-    cost: 38.7,
-    name: "Джон Уик 4"
-},
-{
-    rating: "92%",
-    cost: 38.2,
-    name: "Бэтмен"
-},
-{
-    rating: "92%",
-    cost: 38.3,
-    name: "Тихое место 2"
-},
-{
-    rating: "92%",
-    cost: 36.0,
-    name: "Не время умирать"
-},
-{
-    rating: "91%",
-    cost: 41.5,
-    name: "Заклятие 3: По воле дьявола"
-},
-{
-    rating: "90%",
-    cost: 34.8,
-    name: "Чёрная Вдова"
-},
-{
-    rating: "90%",
-    cost: 39.0,
-    name: "Охотники за привидениями: Наследники"
-},
-{
-    rating: "90%",
-    cost: 34.0,
-    name: "Аватар 2"
-},
-{
-    rating: "88%",
-    cost: 37.6,
-    name: "Неизведанное: Удача Дрейка"
-},
-{
-    rating: "88%",
-    cost: 36.5,
-    name: "Топ Ган: Мэверик"
-},
+const arr1 = [{
+        rating: "96%",
+        cost: 41.3,
+        name: "Дюна"
+    },
+    {
+        rating: "96%",
+        cost: 32.4,
+        name: "Звёздный путь 4"
+    },
+    {
+        rating: "96%",
+        cost: 39.6,
+        name: "Доктор Стрэндж и мультивселенная безумия"
+    },
+    {
+        rating: "96%",
+        cost: 33.7,
+        name: "Круэлла"
+    },
+    {
+        rating: "96%",
+        cost: 33.0,
+        name: "Смерть на Ниле"
+    },
+    {
+        rating: "95%",
+        cost: 38.3,
+        name: "Вечные"
+    },
+    {
+        rating: "94%",
+        cost: 39.2,
+        name: "Матрица 4"
+    },
+    {
+        rating: "94%",
+        cost: 40.8,
+        name: "Главный герой"
+    },
+    {
+        rating: "94%",
+        cost: 41.2,
+        name: "Морбиус"
+    },
+    {
+        rating: "93%",
+        cost: 32.1,
+        name: "Веном 2"
+    },
+    {
+        rating: "93%",
+        cost: 38.7,
+        name: "Джон Уик 4"
+    },
+    {
+        rating: "92%",
+        cost: 38.2,
+        name: "Бэтмен"
+    },
+    {
+        rating: "92%",
+        cost: 38.3,
+        name: "Тихое место 2"
+    },
+    {
+        rating: "92%",
+        cost: 36.0,
+        name: "Не время умирать"
+    },
+    {
+        rating: "91%",
+        cost: 41.5,
+        name: "Заклятие 3: По воле дьявола"
+    },
+    {
+        rating: "90%",
+        cost: 34.8,
+        name: "Чёрная Вдова"
+    },
+    {
+        rating: "90%",
+        cost: 39.0,
+        name: "Охотники за привидениями: Наследники"
+    },
+    {
+        rating: "90%",
+        cost: 34.0,
+        name: "Аватар 2"
+    },
+    {
+        rating: "88%",
+        cost: 37.6,
+        name: "Неизведанное: Удача Дрейка"
+    },
+    {
+        rating: "88%",
+        cost: 36.5,
+        name: "Топ Ган: Мэверик"
+    },
 ].filter(e => (parseInt(e.rating, 10) < 90));
-console.log(arr1);
+
+document.querySelector("#arr").innerHTML = JSON.stringify(arr, null, 2);
+document.querySelector("#arr1").innerHTML = JSON.stringify(arr1, null, 2);
